eaef90b8953f1a56ef6df623fc941aa48db9507f
Hunter Czarapata <czarapata.hunter@gmail.com>
2023-01-17 14:38:06 -0800

    Author is pulling and displaying the authors on the blog posts

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,6 +1,10 @@
 import { client, checkError } from './client.js';
 
 export async function getBlogs() {
-  const resp = await client.from('blogs').select('*');
+  const resp = await client.from('blogs').select('*, authors (*)');
+  //   const blogResp = await client.from('blogs').select('*');
+
+  //   const authResp = await client.from('authors').select('*, authors (*)');
+
   return checkError(resp);
 }
