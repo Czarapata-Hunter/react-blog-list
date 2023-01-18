92b9a44d6e9162df4f8a599d591e86b449ac24f9
Hunter Czarapata <czarapata.hunter@gmail.com>
2023-01-17 16:58:33 -0800

    All 4 tests passing, forgot to unskip the last one

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -2,9 +2,6 @@ import { client, checkError } from './client.js';
 
 export async function getBlogs() {
   const resp = await client.from('blogs').select('*, authors (*)');
-  //   const blogResp = await client.from('blogs').select('*');
-
-  //   const authResp = await client.from('authors').select('*, authors (*)');
 
   return checkError(resp);
 }
